e89a3d1a57e4f00e231c7095de9cf3ac752103ed
Saulo Román Santillán Nava <saulo.santillan@cceo.com.mx>
2026-04-28 11:54:20 -0600

    feat(counter) ✨: Implement counter logic with Vuex

--- 04-vuex-options/src/App.vue
+++ 04-vuex-options/src/App.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <nav class="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+body,
+html {
+  background-color: #f5f5f5;
+  font-family: sans-serif;
+}
+
+.container {
+  width: 1300px;
+  max-width: 95%;
+  margin-inline: auto;
+}
+
+.nav {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  margin: 30px 0;
+}
+
+.nav a {
+  color: blue;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
---
+++ 04-vuex-options/src/components/BtnAumentar.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="incrementarAc(50)">Aumentar</button>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: {
+    ...mapActions(["incrementarAc"]),
+  },
+};
+</script>
